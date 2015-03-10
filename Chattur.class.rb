f36cddeb48db861eccef0adcd0a4fb53b39f881c
Frnkll <spiff@localhost.localdomain>
2015-03-10 22:25:15 +0100

    first concept worked out

--- Chattur.class.rb
+++ Chattur.class.rb
@@ -1,8 +1,30 @@
-# Include the config files
-require 'Chattur.config.rb'
 
-class Chattur_s
-	def initialize(TARGET_ADRESS, TARGET_PORT)
-		@server_address = server_address
+require 'Chattur.config.rb' # The config for this
+require 'socket'      # Sockets are in standard library
+require 'thread'
+
+
+class Chattur
+
+	def initialize
+		puts"Starting Chattur!"
+		@listener_socket = TCPSocket.open(TARGET_ADRESS,TARGET_PORT)
+		@server_socket = TCPServer.open(HOST_PORT)
+		@listner_thread = Thread.new{ loop do listener end }
+		@sender_thread = Thread.new{ loop do sender end }	
+
+
+	end
+
+	def listener
+
+		while line = @listener_socket.gets   # Read lines from the socket
+			puts line.chop      				 # And print with platform line terminator
+		end		
+		puts "listening to: #{TARGET_ADRESS} on port: #{TARGET_PORT}"
+	end
+
+	def sender
+		
 	end
 end
--- Chattur.config.rb
+++ Chattur.config.rb
@@ -1,4 +1,8 @@
 #config file for the application
 
+#Config for the listener thread
+HOST_PORT = "1337"
+
+#Config for the sender thread
 TARGET_ADRESS = "127.0.0.1"
 TARGET_PORT = "12345"
